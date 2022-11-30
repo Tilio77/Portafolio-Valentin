8189844419be0f2a3d71cd29fe6453b328cf28d6
Tilio77 <tiliobolso1@gmail.com>
2022-11-29 21:05:54 -0300

    Experiencia Terminado

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Experience.css';
+
+const Experience = () => {
+	return (
+		<div className="experience">
+			<div className="achievement">
+				<div className="circle">8+</div>
+				<span>Años de </span>
+				<span>Experiencia</span>
+			</div>
+
+			<div className="achievement">
+				<div className="circle">20+</div>
+				<span>Proyectos </span>
+				<span>Completados</span>
+			</div>
+
+			<div className="achievement">
+				<div className="circle">5+</div>
+				<span>Companias de </span>
+				<span>Trabajo</span>
+			</div>
+		</div>
+	)
+}
+
+export default Experience
